20ea72f6a9232ccde91f984f53aa6b4dab0dea01
StefanH <personalnexus@users.noreply.github.com>
2023-08-05 08:59:32 +0200

    added unsubscriptions

--- Program.cs
+++ Program.cs
@@ -10,24 +10,37 @@ var client = new SlimFeed.Protocol.SlimFeed.SlimFeedClient(channel);
 var serverInfo = await client.InitializeAsync(new SlimFeed.Protocol.ClientInfo { Name = "SlimClient"});
 Console.WriteLine($"Connected to {serverInfo.Name}");
 
-// Start subscription requests
 using (var subscription = client.Subscribe())
 {
-    foreach (string id in new[] { "BMW", "Tesla", "Toyota" })
+    var instrumentIds = new[] { "BMW", "Tesla", "Toyota" };
+
+    // Start subscription requests
+    foreach (string id in instrumentIds)
     {
         Console.WriteLine($"Subscribing {id}");
-        await subscription.RequestStream.WriteAsync(new SlimFeed.Protocol.SubscriptionRequest { InstrumentId = id });
+        await subscription.RequestStream.WriteAsync(new SlimFeed.Protocol.SubscriptionRequest { InstrumentId = id, Type = SubscriptionType.Add });
     }
 
     // Process subscription responses
-    while (await subscription.ResponseStream.MoveNext(cancellationTokenSource.Token))
+    int responseCount = 0;
+    while (responseCount < 10 && await subscription.ResponseStream.MoveNext(cancellationTokenSource.Token))
     {
+        responseCount++;
         SubscriptionResponse response = subscription.ResponseStream.Current;
         Console.WriteLine($"{response.InstrumentId}: " + response switch
         {
             { HasError: true } => response.Error,
             _ => $"bid={response.Bid}, ask={response.Ask}, last={response.Last}, currency={response.Currency}"
         });
+    }
+
+    Console.WriteLine($"Stopping subscriptions after {responseCount} responses.");
+
+    // Stop subscriptions
+    foreach (string id in instrumentIds)
+    {
+        Console.WriteLine($"Unsubscribing {id}");
+        await subscription.RequestStream.WriteAsync(new SlimFeed.Protocol.SubscriptionRequest { InstrumentId = id, Type = SubscriptionType.Remove });
     }
 }
 
